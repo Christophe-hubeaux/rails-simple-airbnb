feab1c134ac61884389b60d451295de9c9a8430f
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-06 16:57:34 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.destroy_all
+
+15.times do 
+    Flat.create(
+        name: Faker::Books::Dune.character,
+        address: Faker::Address.full_address,
+        description: Faker::Books::Dune.quote,
+        price_per_night: rand(50...200),
+        number_of_guests: rand(2...10)
+    )
+end
